247f6ceb26f9c7186b3fe821db476fd9b2e6a5c2
Pinchen <Pinchen@auburn.edu>
2020-07-10 18:56:26 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
-docker exec -i test-mysql  mysql -uroot -ppinchen < init.sql
-
+docker exec -i test-mysql mysql -uroot -ppinchen < init.sql
+docker exec -i test-mysql chmod -R +rwx /tmp/Virus
+docker exec -i test-mysql service ssh start
